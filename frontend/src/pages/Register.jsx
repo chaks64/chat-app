b5bf8e49a50b558bbf64fb60ae91ebe6536567bd
chaks64 <chintanbiren.shah@sjsu.edu>
2022-11-18 02:21:23 -0800

    Register FE

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../assets/logo.svg"
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios"
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
-  const handleSubmit = (e) => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    cPassword: "",
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("form");
+    if(handleValidation()){
+        const { password, cPassword, email, username } = values;
+        const {data} = await axios.post(registerRoute,{
+            username,
+            email,
+            password
+        })
+    }
   };
 
   const handleChange = (e) => {
-    // e.preventDefault();
-    // alert("form");
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleValidation = () => {
+    const { password, cPassword, email, username } = values;
+    if (password !== cPassword) {
+      toast.error("Password doesn't match", toastOptions);
+      return false;
+    } else if(username.length < 3){
+        toast.error("Username should be greater than 3", toastOptions);
+      return false;
+    } else if(password.length < 3){
+        toast.error("Password should be greater than 3", toastOptions);
+      return false;
+    } else if(email === ""){
+        toast.error("Email is required", toastOptions);
+      return false;
+    }
+    return true;
   };
 
   return (
@@ -25,7 +68,7 @@ function Register() {
           <input
             type="text"
             placeholder="Username"
-            name="usename"
+            name="username"
             onChange={(e) => handleChange(e)}
           />
           <input
@@ -47,82 +90,83 @@ function Register() {
             onChange={(e) => handleChange(e)}
           />
           <button type="submit">Create User</button>
-          <span>Already have an account? <Link to="/login">Login</Link></span>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
 
 const FormContainer = styled.div`
-height: 100vh;
-width: 100vw;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 1rem;
-align-items: center;
-background-color: #131324;
-.brand{
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
     display: flex;
     align-items: center;
     gap: 1rem;
     justify-content: center;
-    img{
-        height: 5rem;
+    img {
+      height: 5rem;
     }
-    h1{
-        color: white;
-        text-transform: uppercase;
+    h1 {
+      color: white;
+      text-transform: uppercase;
     }
-}
-form{
+  }
+  form {
     display: flex;
     flex-direction: column;
     gap: 2rem;
     background-color: #00000076;
     border-radius: 2rem;
     padding: 3rem 5rem;
-    input{
-        background-color: transparent;
-        padding: 1rem;
-        border: 0.1rem solid #4e0eff;
-        border-radius: 0.4rem;
-        color: white;
-        width: 100%;
-        font-size: 1rem;
-        &:focus{
-            border: 0.1rem solid #997af0;
-            outline: none;
-
-        }
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
     }
-    button{
-        background-color: #997af0;
-        color: white;
-        padding: 1rem 2rem;
-        border: none;
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
         font-weight: bold;
-        cursor: pointer;
-        border-radius: 0.4rem;
-        font-size: 1rem;
-        text-transform: uppercase;
-        transition: 0.5s ease-in-out;
-        &:hover{
-            background-color: #4e0eff;
-        }
+      }
     }
-    span{
-        color: white;
-        text-transform: uppercase;
-        a{
-            color: #4e0eff;
-            text-decoration: none;
-            font-weight: bold;
-
-        }
-    }
-}
+  }
 `;
 
 export default Register;
